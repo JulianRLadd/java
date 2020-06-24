1e1205f8f663749da40c52a2cd795ee8666330e9
SoraVladd <julianrladd@gmail.com>
2020-06-23 20:57:28 -0500

    Finished exercise 2

--- exercise2/Colors.java
+++ exercise2/Colors.java
@@ -1,10 +1,40 @@
-package exercise2;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Colors {
 
     String[] colorful(String[] strings) {
-        // For the given strings, keep all the strings that begin with a color ("red", "yellow", "green", "blue", etc.) return an array of strings that start with a color.  discard all the other strings.  If no strings start with a color in the input array, return an empty array.
-        return new String[]{};
+        // For the given strings, keep all the strings that begin with a color ("red", "yellow", "green", "blue", etc.)
+        // return an array of strings that start with a color.  discard all the other strings.  If no strings start with
+        // a color in the input array, return an empty array.
+        ArrayList<String> colors = new ArrayList<String>();
+        colors.addAll(Arrays.asList(strings));
+        boolean hue = false;
+        String[] checkColor = {"red","blue","black","green","yellow","pink"};
+        for(int i=0;i<colors.size();i++){
+            System.out.println("Checking "+colors.get(i));
+            for(int j=0;j<checkColor.length;j++){
+                if(colors.get(i).contains(checkColor[j])){
+                    System.out.println("Found "+checkColor[j]);
+                    hue = true;
+                    j=checkColor.length;
+                }
+            }
+            if(!hue){
+                colors.remove(i);
+                i--;
+            }
+            hue = false;
+        }
+        String[] checked = new String[colors.size()];
+        checked = colors.toArray(checked);
+        System.out.println("#########################");
+        System.out.println("New List");
+        System.out.println("#########################");
+        for(int i = 0;i<checked.length;i++){
+            System.out.println(checked[i]);
+        }
+        return checked;
     }
 
     public static void main(String[] args) {
--- exercise2/statically.js
+++ exercise2/statically.js
@@ -1,34 +1,44 @@
 class Statically {
-    //fix this code, so all of the instantiation and console.logs() work.  Try not to change the signature of the class, unless absolutely necessary.
-    static #glue = "Epoxy";
-    jello = "Jello";
+  //fix this code, so all of the instantiation and console.logs() work.  Try not to change the signature of the class, unless absolutely necessary.
+  static #glue = "Epoxy";
+  jello = "Jello";
 
-    moveAndShake() {
-        console.log("Never stagnate in life.  Be agile, change, and improve...");
-    }
+  static moveAndShake() {
+    console.log("Never stagnate in life.  Be agile, change, and improve...");
+  }
 
-    static stubborn() {
-        this.moveAndShake();
-        console.log("Please don't make me change... I just don't like it...");
-    }
+  static stubborn() {
+    Statically.moveAndShake();
+    console.log("Please don't make me change... I just don't like it...");
+  }
 
-    static explain() {
-        stubborn();
-        console.log(#glue);
-        //write an explanation for how the keyword static behaves differently in javascript vs. java.
-    }
+  static glue() {
+    console.log(Statically.#glue);
+  }
 
+  static explain() {
+    Statically.stubborn();
+    Statically.glue();
+    //write an explanation for how the keyword static behaves differently in javascript vs. java.
+    console.log(
+      "Static in java is for attributes and methods that belong to the class and not an instance of the class."
+    );
+    console.log(
+      "Static in javascript is for methods that will be the same no matter which class runs them."
+    );
+  }
 }
 
-//get the below code to run correctly without errors:  Make sure you explain what you did in the the explain() method, defined in the class.  Do NOT remove the 'static' keyword from the explain() method.
+//get the below code to run correctly without errors:  Make sure you explain what you did in the the explain()
+//method, defined in the class.  Do NOT remove the 'static' keyword from the explain() method.
 
 const stats = new Statically();
 
-stats.moveAndShake();
-stubborn();
-stats.explain();
+Statically.moveAndShake();
+Statically.stubborn();
+Statically.explain();
 
-console.log(stats.#glue);
+Statically.glue();
 console.log(stats.jello);
 
 //Once you have finished getting statistically.js to run, refactor the code here in Java.
